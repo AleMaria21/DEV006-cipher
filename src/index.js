7d2bb7488d1b12cb5c9cb74b31d4b307f477c1fa
AleMaria21 <127411579+AleMaria21@users.noreply.github.com>
2023-03-24 13:19:18 -0500

    cambio cipher

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+window.alert="hoguera"
+const confirmBtn = hoguera.querySelector('conocimiento');
+
+const alfabeto=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+let botoncifrar=document.getElementById("botoncifrar");
+let eInput=document.getElementById("cifrado");
+let pInput=document.getElementById("cifrar");
+let inputs= [cifrado, cifrar]
+
+inputs.forEach(input =>{
+    input.oninput=()=>{
+        input.value=input.value.toUpperCase()
+    }
+});
+
+botoncifrar.addEventListener("click", 
+    function () {
+     let pInput = document.getElementById("cifrar").value;
+     let num = document.getElementById("numcifrar").value;
+     let eInput = document.getElementById("cifrado");
+     eInput.innerHTML = mcifrar(pInput, num);
+}); 
+    
+    function mcifrar(pInput, num) {
+         var resultado = "";
+         for(var i=0; i<pInput.length; i++){
+             var ascii_num=pInput.charCodeAt(i);
+             if(ascii_num >= 65 && ascii_num <=90){
+                 resultado += String.fromCharCode(((pInput - 65 + parseInt(num)) % 26) +65);
+                }
+             else{
+                 resultado += pInput.charAt(i);
+                }
+        
+            }
+            return resultado;
+    }
+
+// console.log(cipher);
