3bf4e445f381f6fc23fb0035b1d6e77ee6cb5122
AleMaria21 <127411579+AleMaria21@users.noreply.github.com>
2023-03-26 13:15:15 -0500

    Initial commit

--- src/cipher.js
+++ src/cipher.js
@@ -3,16 +3,16 @@ const cipher = {
   encode: function (offset, string) {
     
     let resultadoci = "";
-    
+
     for (var i = 0; i < string.length; i++) {
-      const ascii_num = string.charCodeAt(i);
+      const ascii_num = String.charCodeAt(i);
       if (ascii_num >= 65 && ascii_num <= 90) {
-        inputcifrado += String.fromCharCode(((ascii_num - 65 + offset) % 26) + 65);
-        console.log (ascii_num)
+        resultadoci += string.fromCharCode(((ascii_num - 65 + offset) % 26) + 65);
+        console.log (ascii_num);
       }
 
       else {
-       resultadoci += string.charAt(i);
+       resultadoci += String.charAt(i);
       }
 
     }
