fddfb04eb09d9cce30a96cf3c49483b5e01243d1
AleMaria21 <127411579+AleMaria21@users.noreply.github.com>
2023-03-26 12:41:09 -0500

    cambio cipher

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,25 @@
 const cipher = {
   // ...
+  encode: function (offset, string) {
+    
+    let resultadoci = "";
+    
+    for (var i = 0; i < string.length; i++) {
+      const ascii_num = string.charCodeAt(i);
+      if (ascii_num >= 65 && ascii_num <= 90) {
+        inputcifrado += String.fromCharCode(((ascii_num - 65 + offset) % 26) + 65);
+        console.log (ascii_num)
+      }
+
+      else {
+       resultadoci += string.charAt(i);
+      }
+
+    }
+    return resultadoci;
+  }
+
+
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,42 +1,30 @@
 import cipher from './cipher.js';
 
+console.log(cipher);
+
 window.alert="hoguera"
 const confirmBtn = hoguera.querySelector('conocimiento');
 
 const alfabeto=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
 let botoncifrar=document.getElementById("botoncifrar");
-let eInput=document.getElementById("cifrado");
-let pInput=document.getElementById("cifrar");
+let resultadoci = document.getElementById("cifrado");
+let string=document.getElementById("cifrar"). value;
 let inputs= [cifrado, cifrar]
 
-inputs.forEach(input =>{
+inputs.forEach( input =>{
     input.oninput=()=>{
         input.value=input.value.toUpperCase()
     }
 });
 
-botoncifrar.addEventListener("click", 
-    function () {
-     let pInput = document.getElementById("cifrar").value;
-     let num = document.getElementById("numcifrar").value;
-     let eInput = document.getElementById("cifrado");
-     eInput.innerHTML = mcifrar(pInput, num);
+botoncifrar.addEventListener("click",   
+ function() {
+    const llave = document.getElementById("numcifrar").value;
+    const offset = parseInt (llave);
+    resultadoci.innerHTML = cipher.encode(offset, string);
+   
 }); 
     
-    function mcifrar(pInput, num) {
-         var resultado = "";
-         for(var i=0; i<pInput.length; i++){
-             var ascii_num=pInput.charCodeAt(i);
-             if(ascii_num >= 65 && ascii_num <=90){
-                 resultado += String.fromCharCode(((pInput - 65 + parseInt(num)) % 26) +65);
-                }
-             else{
-                 resultado += pInput.charAt(i);
-                }
-        
-            }
-            return resultado;
-    }
 
-// console.log(cipher);
+console.log(cipher);
