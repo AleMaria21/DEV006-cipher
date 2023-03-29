0392ef7eb303fb9c525409190193e009d7075e02
AleMaria21 <127411579+AleMaria21@users.noreply.github.com>
2023-03-29 12:27:06 -0500

    Initial commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,22 +1,20 @@
 
 const cipher = {
- encode: function (offset, string) {
+  encode: function (offset, string) {
     let resultadoci = "";
     for (let i = 0; i < string.length; i++) {
       const ascii_num = string.charCodeAt(i);
       if (ascii_num >= 65 && ascii_num <= 90) {
-         resultadoci += String.fromCharCode(((ascii_num - 65 + offset) % 26) + 65);
-        }
-
+        resultadoci += String.fromCharCode(((ascii_num - 65 + offset) % 26) + 65);
+      }
       else {
-         resultadoci += string.charAt(i);
-        }
-
+        resultadoci += string.charAt(i);
+      } 
     }
     return (document.getElementById("cifrado").value = resultadoci);
   },     
 
- decode: function (offsetdes, stringdes) {
+  decode: function (offsetdes, stringdes) {
     let resultadodes = "";
     for (let i = 0; i < stringdes.length; i++) {
       const ascii_de = stringdes.charCodeAt(i);
--- src/index.js
+++ src/index.js
@@ -1,39 +1,53 @@
 import cipher from './cipher.js';
 
-window.alert="hoguera"
-const confirmBtn = hoguera.querySelector('conocimiento');
+window.alert = "hoguera"
 
-let inputs = [cifrado, cifrar]
-inputs.forEach(input =>{
-    input.oninput=()=>{
-        input.value=input.value.toUpperCase()
-    }
+const inputs = [cifrado, cifrar]
+inputs.forEach(input => { 
+  input.oninput = () => {
+    input.value = input.value.toUpperCase()
+  }
 });
 
-let inputsdes = [descifrado, descifrar]
-inputsdes.forEach(input =>{
-    input.oninput=()=>{
-        input.value=input.value.toUpperCase()
-    }
+const inputsdes = [descifrado, descifrar]
+inputsdes.forEach(input => {
+  input.oninput = () => {
+    input.value = input.value.toUpperCase()
+  }
 });
 
-botoncifrar.addEventListener("click",   
- function() {
+botoncifrar.addEventListener("click",
+  function () {
     const offset = parseInt(document.getElementById("numcifrar").value);
     const string = document.getElementById("cifrar").value;
-    resultado= cipher.encode(offset, string);
-    let resultado = document.getElementById("cifrado").value;
-    }
-); 
+    const resultado = document.getElementById("cifrado").value;
+    resultado = cipher.encode(offset, string);
+  }
+);
 
-botondescifrar.addEventListener("click",   
- function() {
-     const offsetdes = parseInt(document.getElementById("numdescifrar").value);
-     const stringdes = document.getElementById("descifrar").value;
-     resultadodes = cipher.decode(offsetdes, stringdes);
-     let resultadodes = document.getElementById("descifrado").value;
-    }
-); 
+botondescifrar.addEventListener("click",
+  function () {
+    const offsetdes = parseInt(document.getElementById("numdescifrar").value);
+    const stringdes = document.getElementById("descifrar").value;
+    const resultadodes = document.getElementById("descifrado").value;
+    resultadodes = cipher.decode(offsetdes, stringdes);
+  }
+);
 
+document.getElementById("resetci").addEventListener("click",
+  function () {
+    document.getElementById("cifrado").value = "";
+    document.getElementById("numcifrar").value = "";
+    document.getElementById("cifrar").value = "";
+  }
+);
+
+document.getElementById("resetdes").addEventListener("click",
+  function () {
+    document.getElementById("descifrar").value = "";
+    document.getElementById("numdescifrar").value = "";
+    document.getElementById("descifrado").value = "";
+  }
+);
 
 console.log(cipher);
